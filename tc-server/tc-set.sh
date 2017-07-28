2824d9f2f99296f26311bb422c12c290647b0691
Miguel Ventura <miguel.ventura@gmail.com>
2017-07-28 16:53:52 +0100

    copy copy paste paste bug bug

--- tc-server/tc-set.sh
+++ tc-server/tc-set.sh
@@ -60,7 +60,7 @@ tc qdisc add dev wlan0 parent 1:1 handle 10: prio
 tc qdisc add dev wlan0 parent 1:2 handle 2: netem delay $DOWNSTREAM_DELAY
 tc qdisc add dev wlan0 parent 2:1 handle 20: htb default 1
 tc class add dev wlan0 parent 20:1 classid 20:1 htb rate $DOWNSTREAM_BANDWIDTH burst 15k
-tc filter add dev eth0 protocol ip parent 1: prio 1 u32 match ip sport 443 0xffff flowid 1:2
-tc filter add dev eth0 protocol ip parent 1: prio 1 u32 match ip sport 80 0xffff flowid 1:2
+tc filter add dev wlan0 protocol ip parent 1: prio 1 u32 match ip sport 443 0xffff flowid 1:2
+tc filter add dev wlan0 protocol ip parent 1: prio 1 u32 match ip sport 80 0xffff flowid 1:2
 
 echo "success"
