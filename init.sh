1c3bd180633773755b2823def4d168e922f3b653
Miguel Ventura <miguel.ventura@gmail.com>
2017-07-28 18:13:05 +0100

    bugfix auto-upgrade

--- init.sh
+++ init.sh
@@ -5,12 +5,21 @@ set -e
 MYDIR="$(dirname "$(readlink -f "$0")")"
 LOGFILE="$MYDIR/lastoutput"
 
+cd "$MYDIR"
 rm -f "$LOGFILE"
 
 # attempt to fetch the latest version
-{
-    git && git pull --rebase --stat origin master || true
-} >>"$LOGFILE" 2>&1
+if git >/dev/null 2>&1; then
+    if [ ! \
+        "$(git rev-parse master)" = \
+        "$(git rev-parse "master@{u}")"
+    ]; then
+        git pull --rebase --stat origin master && \
+        {
+            cd "$MYDIR/tc-server" && npm install
+        } || true;
+    fi
+fi >>"$LOGFILE" 2>&1
 
 # start the server
 ( cd "$MYDIR/tc-server" && nohup setsid ~/bin/node app.js & )
